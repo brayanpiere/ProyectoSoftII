dd333f6063b1278cac2863ca994110c0913d851a
brayan <brayanpiere@gmail.com>
2023-10-10 20:34:18 -0500

    script de BD agregada

--- src/main/java/dao/mongodb/ConexionMongoDB.java
+++ src/main/java/dao/mongodb/ConexionMongoDB.java
@@ -4,10 +4,18 @@
  */
 package dao.mongodb;
 
+import conexion.Conexion;
+import java.sql.Connection;
+
 /**
  *
  * @author orope
  */
-public class ConexionMongoDB {
+public class ConexionMongoDB implements Conexion{
+
+    @Override
+    public Connection getConexion() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
